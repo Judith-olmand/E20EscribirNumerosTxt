a4e766f5e1036a1bfdd9688e2ba872314f4c8ae9
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-10 09:27:06 +0100

    acabado

--- src/E20EscribirNumerosTxt.java
+++ src/E20EscribirNumerosTxt.java
@@ -1,13 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class E20EscribirNumerosTxt  {
+	public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<Integer>();
+
+        for (int n = 1; n <= 10; n++) {
+            numeros.add(n);
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("numeros.txt"))) {
+            for (int num : numeros) {
+                writer.write(num + "\r");
+            }
+
+            System.out.println("Líneas escritas en el archivo.");
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al escribir en el archivo: " +
+                    e.getMessage());
+        }
+    }
 }
--- src/E20LeerNumerosTxt.java
+++ src/E20LeerNumerosTxt.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class E20LeerNumerosTxt {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("numeros.txt"))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo: " +
+                    e.getMessage());
+        }
+    }
 }
